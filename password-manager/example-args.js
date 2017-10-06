32ec5c065700c1724c5fdcd221cdc61e37e9d317
Mark Christenson <mark.christenson@ticketmaster.com>
2017-10-06 15:31:46 -0700

    sec 4-28: validating and requiring input with yargs

--- password-manager/example-args.js
+++ password-manager/example-args.js
@@ -1,4 +1,20 @@
-var argv = require("yargs").argv;
+var argv = require("yargs")
+  .command('hello', 'greets the user', function (yargs) {
+    yargs.options({
+      name: {
+        demand: true,
+        alias: 'n',
+        description: 'your first name goes here'
+      },
+      lastname: {
+        demand: true,
+        alias: 'l',
+        description: 'your last name goes here'
+      }
+    });
+  })
+  .help('help')
+  .argv;
 var command = argv._[0];
 
 console.log(argv);
