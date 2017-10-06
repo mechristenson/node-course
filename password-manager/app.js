a89b9be20707437ec793605733d6c62fa3b76d66
Mark Christenson <mark.christenson@ticketmaster.com>
2017-10-06 15:59:07 -0700

    sec 4-30: set up create and get account commands for password manager

--- password-manager/app.js
+++ password-manager/app.js
@@ -3,6 +3,44 @@ console.log('starting password manager');
 var storage = require('node-persist');
 storage.initSync();
 
+var argv = require('yargs')
+  .command('create', 'creates a new account', function (yargs) {
+    yargs.options({
+      name: {
+        demand: true,
+        alias: 'n',
+        description: 'account name (e.g. twitter, facebook)',
+        type: 'string'
+      },
+      username: {
+        demand: true,
+        alias: 'u',
+        description: 'account username or email',
+        type: 'string'
+      },
+      password: {
+        demand: true,
+        alias: 'p',
+        description: 'account password',
+        type: 'string'
+      }
+    }).help('help');
+  })
+  .command('get', 'get an existing account', function (yargs) {
+    yargs.options({
+      name: {
+        demand: true,
+        alias: 'n',
+        description: 'your account name',
+        type: 'string'
+      }
+    }).help('help');
+  })
+  .help('help')
+  .argv;
+
+var command = argv._[0];
+
 function createAccount (account) {
   var accounts = storage.getItemSync('accounts');
 
@@ -27,4 +65,23 @@ function getAccount (accountName) {
   })
 
   return matchedAccount;
+}
+
+if (command === 'create') {
+  var createdAccount = createAccount({
+    name: argv.name,
+    username: argv.username,
+    password: argv.password
+  });
+  console.log('Account created');
+  console.log(createdAccount);
+} else if (command === 'get') {
+  var fetchedAccount = getAccount(argv.name);
+
+  if (typeof fetchedAccount === 'undefined') {
+    console.log('Account not found');
+  } else {
+    console.log('Account found');
+    console.log(fetchedAccount);
+  }
 }
