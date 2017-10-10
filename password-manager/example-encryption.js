dc0d675a111c44d6ba50f66daff62fae56c1c987
Mark Christenson <mark.christenson@ticketmaster.com>
2017-10-09 21:40:58 -0700

    sec 4-32: encrypting information

---
+++ password-manager/example-encryption.js
@@ -0,0 +1,17 @@
+var crypto = require('crypto-js');
+
+var secretMessage = {
+  name: 'Mark',
+  secretName: 'C'
+};
+var secretKey = '123abc';
+
+// Encrypt
+var encryptedMessage = crypto.AES.encrypt(JSON.stringify(secretMessage), secretKey);
+console.log('Encrypted Message: ' + encryptedMessage);
+
+// Decrypt
+var bytes = crypto.AES.decrypt(encryptedMessage, secretKey);
+var decryptedMessage = JSON.parse(bytes.toString(crypto.enc.Utf8));
+
+console.log('Decrypted Message: ' + decryptedMessage.secretName);
