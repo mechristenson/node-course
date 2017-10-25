d5d7fc0985afc0696c9ec6ba3250e1e6877e7794
Mark Christenson <mark.christenson@ticketmaster.com>
2017-10-25 16:37:11 -0700

    sec 6-49: refactor example web server middleware

--- async/hello-async.js
+++ async/hello-async.js
@@ -1,13 +1,3 @@
-// setTimeout(function () {
-//   console.log('1')
-// }, 2000);
-//
-// setTimeout(function () {
-//   console.log('3')
-// }, 1000);
-//
-// console.log('2');
-
 console.log('Challenge');
 
 function printInTwoSeconds (message) {
