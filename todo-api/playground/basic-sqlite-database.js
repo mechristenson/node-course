8f4427a1bc0641d22eb75d6d62ffd30e81dbe2b9
Mark Christenson <mark.evan.christenson@gmail.com>
2017-11-14 18:39:46 -0800

    sec 8-66: add model validation with sequelize

--- todo-api/playground/basic-sqlite-database.js
+++ todo-api/playground/basic-sqlite-database.js
@@ -6,21 +6,58 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
 
 var Todo = sequelize.define('todo', {
   description: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: [1, 250]
+    }
   },
   completed: {
-    type: Sequelize.BOOLEAN
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
   }
-})
+});
 
-sequelize.sync().then(function () {
+sequelize.sync({
+  // force: true
+}).then(function () {
   console.log('Everything is synced');
 
-  Todo.create({
-    description: 'Walk my dog',
-    completed: false
-  }).then(function (todo) {
-    console.log('Finished!');
-    console.log(todo);
-  })
-})
+  var id = 3;
+
+  Todo.findById(id).then(function (todo) {
+    if (todo) {
+      console.log(todo.toJSON());
+    } else {
+      console.log('Error could not find item with id: ' + id)
+    }
+  });
+
+  // Todo.create({
+  //   description: 'take out trash'
+  // }).then(function (todo) {
+  //   return Todo.create({
+  //     description: 'clean office'
+  //   });
+  // }).then(function() {
+  //   // return Todo.findById(1)
+  //   return Todo.findAll({
+  //     where: {
+  //       description: {
+  //         $like: '%Office%'
+  //       }
+  //     }
+  //   });
+  // }).then(function(todos) {
+  //   if (todos) {
+  //     todos.forEach(function (todo) {
+  //       console.log(todo.toJSON());
+  //     });
+  //   } else {
+  //     console.log('no todos found');
+  //   }
+  // }).catch(function (e) {
+  //   console.log(e);
+  // });
+});
