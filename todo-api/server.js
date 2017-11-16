9daac2ebb51a37191f253203ff74293e25c8dd11
Mark Christenson <mark.evan.christenson@gmail.com>
2017-11-16 11:51:01 -0800

    chore: refactor functions

--- todo-api/server.js
+++ todo-api/server.js
@@ -10,12 +10,12 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.send('Todo API Root');
 });
 
 // GET /todos?completed=<boolean>&q=<string>
-app.get('/todos', function (req, res) {
+app.get('/todos', function(req, res) {
   var query = req.query;
   var where = {};
   var filteredTodos = [];
@@ -32,48 +32,48 @@ app.get('/todos', function (req, res) {
     };
   }
 
-  db.todo.findAll({where: where}).then(function (todos) {
+  db.todo.findAll({where: where}).then(function(todos) {
     res.json(todos);
-  }).catch(function (e) {
+  }).catch(function(e) {
     res.status(500).send();
   });
 });
 
 // GET /todos/:id
-app.get('/todos/:id', function (req, res) {
+app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
 
-  db.todo.findById(todoId).then(function (todo) {
+  db.todo.findById(todoId).then(function(todo) {
     if (!!todo) {
       res.json(todo.toJSON());
     } else {
       res.status(404).send();
     }
-  }, function (e) {
+  }, function(e) {
     res.status(500).send();
   });
 });
 
 // POST /todos
-app.post('/todos', function (req, res) {
+app.post('/todos', function(req, res) {
   var body = _.pick(req.body, 'description', 'completed');
 
-  db.todo.create(body).then(function (todo) {
+  db.todo.create(body).then(function(todo) {
     res.json(todo.toJSON());
-  }).catch(function (e) {
+  }, function(e) {
     res.status(400).json(e);
   });
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', function (req, res) {
+app.delete('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
 
   db.todo.destroy({
     where: {
       id: todoId
     }
-  }).then(function (rowsDeleted) {
+  }).then(function(rowsDeleted) {
     if (rowsDeleted === 0) {
       res.status(404).json({
         error: 'No todo with that id'
@@ -81,13 +81,13 @@ app.delete('/todos/:id', function (req, res) {
     } else {
       res.status(204).send();
     }
-  }, function () {
+  }, function() {
     res.status(500).send();
   });
 });
 
 // PUT /todos/:id
-app.put('/todos/:id', function (req, res) {
+app.put('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
   var body = _.pick(req.body, 'description', 'completed');
   var attributes = {};
@@ -100,17 +100,17 @@ app.put('/todos/:id', function (req, res) {
     attributes.description = body.description;
   }
 
-  db.todo.findById(todoId).then(function (todo) {
+  db.todo.findById(todoId).then(function(todo) {
     if (todo) {
-      todo.update(attributes).then(function (todo) {
+      todo.update(attributes).then(function(todo) {
         res.json(todo.toJSON());
-      }, function (e) {
+      }, function(e) {
         res.status(400).json(e);
       });
     } else {
       res.status(404).send();
     }
-  }, function () {
+  }, function() {
     res.status(500).send();
   });
 });
