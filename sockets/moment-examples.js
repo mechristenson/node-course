224037eda884fc7c3b280f9030e3f757fce362cf
Mark Christenson <mark.evan.christenson@gmail.com>
2017-12-01 15:04:07 -0800

    sec 10-93: more examples of manipulating timestamps with momentjs

--- sockets/moment-examples.js
+++ sockets/moment-examples.js
@@ -2,8 +2,15 @@ var moment = require('moment');
 var now = moment();
 
 console.log(now.format());
+console.log(now.format('X'));
+console.log(now.valueOf());
 
-now.subtract(1, 'year');
+var timestamp = 1512169024792;
+var timestampMoment = moment.utc(timestamp);
 
-console.log(now.format());
-console.log(now.format('MMM Do YYYY, h:mma')); // Oct 5th 2015, 6:45pm
+console.log(timestampMoment.local().format('h:mm a')); // 11:06 am
+
+// now.subtract(1, 'year');
+//
+// console.log(now.format());
+// console.log(now.format('MMM Do YYYY, h:mma')); // Oct 5th 2015, 6:45pm
