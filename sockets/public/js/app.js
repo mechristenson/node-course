da24d2276b4523dc39bb12a8297ef2adcddc8d95
Mark Christenson <mark.evan.christenson@gmail.com>
2017-12-03 15:30:10 -0800

    sec 10-99: add backed support for chat rooms
    - set up joinRoom socket method to inform users of a room when someone joins
    - hide messages from all users in different rooms

--- sockets/public/js/app.js
+++ sockets/public/js/app.js
@@ -8,6 +8,11 @@ jQuery('.room-title').text(room);
 
 socket.on('connect', function () {
   console.log('Connected to socket.io server!');
+
+  socket.emit('joinRoom', {
+    name: name,
+    room: room
+  });
 });
 
 socket.on('message', function (message) {
