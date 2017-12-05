222e413905914ae3a1c42f2af58abe7e3247a1d8
Mark Christenson <mark.evan.christenson@gmail.com>
2017-12-05 13:04:34 -0800

    sec 11-113: debugging example

---
+++ es6-features/features/debugger.js
@@ -0,0 +1,14 @@
+var name = 'Mark';
+var a = 12;
+var b = 22;
+
+//console.log(name);
+debugger; // run 'node debug <filename>' to use
+
+// debugger commands
+// c - continue
+// repl - execute js
+// list(<N>) - list next N lines
+// restart - restart process
+
+// node-inspector debugger: https://nodejs.org/api/debugger.html#debugger_v8_inspector_integration_for_node_js
